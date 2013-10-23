385e71366d46d122dfbdf3ed8a14b1b1aa93ef13
alexcpp2013 <alex.melnik@iua>
2013-10-23 19:22:27 +0300

    First Commit

---
+++ src/VerifySiteAttributes/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace VerifySiteAttributes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btNUnit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbSites = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.bClose = new System.Windows.Forms.Button();
+            this.ofdSites = new System.Windows.Forms.OpenFileDialog();
+            this.ofdAttributes = new System.Windows.Forms.OpenFileDialog();
+            this.rtbReport = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btNUnit);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tbSites);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(791, 91);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Файлы с данными";
+            // 
+            // btNUnit
+            // 
+            this.btNUnit.Location = new System.Drawing.Point(704, 53);
+            this.btNUnit.Name = "btNUnit";
+            this.btNUnit.Size = new System.Drawing.Size(75, 23);
+            this.btNUnit.TabIndex = 17;
+            this.btNUnit.Text = "Загрузить";
+            this.btNUnit.UseVisualStyleBackColor = true;
+            this.btNUnit.Click += new System.EventHandler(this.btNUnit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Введите имя файла с сайтами";
+            // 
+            // tbSites
+            // 
+            this.tbSites.Location = new System.Drawing.Point(15, 56);
+            this.tbSites.Name = "tbSites";
+            this.tbSites.ReadOnly = true;
+            this.tbSites.Size = new System.Drawing.Size(644, 20);
+            this.tbSites.TabIndex = 18;
+            this.tbSites.Text = "sites.xml";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rtbReport);
+            this.groupBox2.Controls.Add(this.progressBar1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(0, 97);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(791, 380);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Обработка данных";
+            //this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(630, 63);
+            this.progressBar1.MarqueeAnimationSpeed = 25;
+            this.progressBar1.Maximum = 25;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(155, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 4;
+            this.progressBar1.UseWaitCursor = true;
+            this.progressBar1.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Отчет";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.bClose);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 490);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(791, 30);
+            this.panel1.TabIndex = 2;
+            // 
+            // bClose
+            // 
+            this.bClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.bClose.Dock = System.Windows.Forms.DockStyle.Right;
+            this.bClose.Location = new System.Drawing.Point(712, 0);
+            this.bClose.Name = "bClose";
+            this.bClose.Size = new System.Drawing.Size(75, 26);
+            this.bClose.TabIndex = 0;
+            this.bClose.Text = "Выход";
+            this.bClose.UseVisualStyleBackColor = true;
+            this.bClose.Click += new System.EventHandler(this.bClose_Click);
+            // 
+            // ofdSites
+            // 
+            this.ofdSites.FileName = "openFileDialog1";
+            this.ofdSites.Filter = "xml файл (*.xml) | *.xml";
+            // 
+            // ofdAttributes
+            // 
+            this.ofdAttributes.FileName = "openFileDialog1";
+            this.ofdAttributes.Filter = "xml файл (*.xml) | *.xml";
+            // 
+            // rtbReport
+            // 
+            this.rtbReport.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.rtbReport.Location = new System.Drawing.Point(3, 92);
+            this.rtbReport.Name = "rtbReport";
+            this.rtbReport.ReadOnly = true;
+            this.rtbReport.Size = new System.Drawing.Size(785, 285);
+            this.rtbReport.TabIndex = 0;
+            this.rtbReport.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.bClose;
+            this.ClientSize = new System.Drawing.Size(791, 520);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Проверка Блокировки Сайта Google";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button bClose;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btNUnit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbSites;
+        private System.Windows.Forms.OpenFileDialog ofdSites;
+        private System.Windows.Forms.OpenFileDialog ofdAttributes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.RichTextBox rtbReport;
+    }
+}
+
---
+++ src/VerifySiteAttributes/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VerifySiteAttributes
+{
+    public partial class Form1 : Form
+    {
+        List<string> Sites = new List<string>();
+        string FileSites = "";
+
+        WebBrowser Web = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //WebBrowser async default
+
+            if (tbSites.Text == "")
+            {
+                MessageBox.Show("Введите имя файла для парсинга. ", 
+                                "Информационное сообщение", 
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            Initialize();
+
+            try
+            {
+                progressBar1.Visible = true;
+                var rs = new XmlReaderConfig();
+                var ra = new XmlReaderConfig();
+                rs.GetParameters(FileSites, Sites, "site");
+                string title = "Предупреждение о вредоносном ПО";
+                string content = "";
+
+                ClearWebBrowser();
+                using (Web = new WebBrowser())
+                {
+                    SetWebBrowserOptions();
+                    rtbReport.Text += "Заблокирвоаны сайты: \n\n";
+                    foreach (var s in Sites)
+                    {
+                        string newaddress = s;
+                        if (!s.StartsWith("http://") &&
+                            !s.StartsWith("https://"))
+                        {
+                            newaddress = "http://" + s;
+                        }
+                        LoadSite("https://www.google.com.ua/interstitial?url=" +
+                                 newaddress);
+
+                        //or Like str OR Containe str
+                        string tmp = GetDocumentTitle();
+                        var t = Web.Document;
+                        if (tmp == title)
+                            content += "\nСайт: " + s;
+
+                        rtbReport.Text += content;
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Возникла ошибка:  \n" + err.Message,
+                                "Ошибка",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                progressBar1.Visible = false;
+            }
+            
+            MessageBox.Show("Проверка закончена\n",
+                            "Информационное сообщение",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        protected string GetDocumentTitle()
+        {
+            return Web.DocumentTitle;
+        }
+
+        private void Initialize()
+        {
+            rtbReport.Clear();
+            Sites.Clear();
+            FileSites = tbSites.Text;
+        }
+
+        private void btNUnit_Click(object sender, EventArgs e)
+        {
+            if (ofdSites.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    tbSites.Text = ofdSites.FileName;
+                }
+                catch (Exception ex)
+                {
+                    tbSites.Text = "";
+                    MessageBox.Show("Hе возможно считать файл: " + ex.Message,
+                                    "Ошибка",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            Refresh();
+        }
+
+        protected bool Navigate(String address)
+        {
+            if (String.IsNullOrEmpty(address)) return false;
+            if (address.Equals("about:blank")) return false;
+            if (!address.StartsWith("http://") &&
+                !address.StartsWith("https://"))
+            {
+                address = "http://" + address;
+            }
+
+            try
+            {
+                Web.Navigate(new Uri(address));
+                return true;
+            }
+            catch (System.UriFormatException err)
+            {
+                return false;
+            }
+            catch (Exception err)
+            {
+                return false;
+            }
+        }
+
+        private void SetWebBrowserOptions()
+        {
+            Web.ScriptErrorsSuppressed = true;
+            Web.Visible = false;
+        }
+
+        protected void ClearWebBrowser()
+        {
+            if (Web != null)
+                Web.Dispose();
+            Web = null;
+        }
+
+        protected void LoadSite(string url)
+        {
+            if (Navigate(url) != true)
+            {
+                throw(new Exception("Не корректный url."));
+            }
+
+            while (Web.ReadyState != WebBrowserReadyState.Complete)
+                Application.DoEvents();
+        }
+    }
+}
